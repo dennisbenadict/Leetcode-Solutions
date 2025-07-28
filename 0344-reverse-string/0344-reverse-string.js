ffdbb368bc8d2644803966b5ccd767f5550ebbfa
Dennis Benadict <dennisbenadict123@gmail.com>
2025-07-28 12:26:53 +0530

    Time: 0 ms (100%), Space: 58.7 MB (98.33%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  for(i=s.length-1;i>=0;i--){
+    return s.reverse()
+}
+};
