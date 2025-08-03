5db1b17db03c61b384a7f8f4f150bee74a88cd04
Dennis Benadict <dennisbenadict123@gmail.com>
2025-08-03 18:21:05 +0530

    Time: 0 ms (100%), Space: 52.5 MB (95.45%) - LeetHub

---
+++ 2556-convert-the-temperature/2556-convert-the-temperature.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+let ans=[]
+let Kelvin=celsius+273.15
+let Fahrenheit=celsius*1.80+32.00
+ans.push(Kelvin,Fahrenheit)
+    return ans
+};
