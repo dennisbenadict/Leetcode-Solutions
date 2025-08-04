0439ffcf8a5ed31e77568c5328110e9a07f02b52
Dennis Benadict <dennisbenadict123@gmail.com>
2025-08-04 15:24:24 +0530

    Time: 4 ms (90.15%), Space: 64.8 MB (41.72%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,15 +2,38 @@
  * @param {number} x
  * @return {boolean}
  */
+// var isPalindrome = function(x) {
+//     y=String(x)
+//     k=String(x)
+//     let arr1=k.split("").join()
+//     let arr=y.split("").reverse().join()
+//     if(arr1==arr){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// };
+
+
+
+
 var isPalindrome = function(x) {
-    y=String(x)
-    k=String(x)
-    let arr1=k.split("").join()
-    let arr=y.split("").reverse().join()
-    if(arr1==arr){
-        return true
+    let y=String(x)
+    last = y.length-1
+    for(i=0;i<y.length;i++){
+  
+     
+      if(last==i){
+         return true
+     }
+     if(y[last]!=y[i]){
+        
+         return false
+     }
+     last--
+   
     }
-    else{
-        return false
-    }
+    
+    return true
 };
