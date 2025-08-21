6da39785a542b36f083fc8e37c50e56fcb8cfbcc
Dennis Benadict <dennisbenadict123@gmail.com>
2025-08-21 19:03:52 +0530

    Time: 0 ms (100%), Space: 53.8 MB (54.13%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let index=haystack.indexOf(needle)
+    return index
+    //  let index=0
+    //  let spl=haystack.split(needle)
+    // for(i=0;i<=spl.length-1;i++){
+    //     if(spl[i]==needle){
+    //         index=i
+    //     }
+    //     else{
+    //         index=-1
+    //     }
+    // }
+    // return index
+};
+
+
