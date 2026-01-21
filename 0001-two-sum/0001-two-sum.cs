7d5cef7cf50b9e4e7367651ff8267a9cdc58d411
Dennis Benadict <dennisbenadict123@gmail.com>
2026-01-21 14:57:50 +0530

    Time: 30 ms (27.14%), Space: 48.8 MB (60.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,12 +1,15 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
+        List<int>result=new List<int>();
         for(int i=0;i<nums.Length;i++){
             for(int j=i+1;j<nums.Length;j++){
                 if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
+                    result.Add(i);
+                    result.Add(j);
+                    return result.ToArray();
                 }
             }
         }
-        return new int[] { };
+        return Array.Empty<int>();
     }
 }
